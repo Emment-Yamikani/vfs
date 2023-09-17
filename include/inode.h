f22027c4592db7b02dd39c92ffb95c5a4c5cafbd
Emment Yamikani Banda <emmentyamikani@gmail.com>
2023-09-17 13:02:55 +0200

    Basic inode generics added.

--- include/inode.h
+++ include/inode.h
@@ -3,17 +3,19 @@
 #include <stdlib.h>
 #include <stddef.h>
 #include <spinlock.h>
+#include <sys/types.h>
 
 struct iops;
+struct dentry;
 
 typedef enum {
     FS_INV,
     FS_REG,
     FS_DIR,
     FS_CHR,
-    FS_FIFO,
     FS_SYM,
     FS_BLK,
+    FS_FIFO,
 } itype_t;
 
 typedef struct {
@@ -28,23 +30,26 @@ typedef struct {
 } inode_t;
 
 typedef struct iops {
-    int     (*link)();
-    int     (*close)();
-    ssize_t (*read)(inode_t *ip, __off_t off, void *buf, size_t nb);
-    ssize_t (*write)(inode_t *ip, __off_t off, void *buf, size_t nb);
-    int     (*mknod)();
-    int     (*fnctl)();
-    int     (*ioctl)();
-    int     (*mkdir)();
-    int     (*unlink)();
-    int     (*lookup)();
-    int     (*create)();
-    int     (*rename)();
-    int     (*readdir)();
-    int     (*symlink)();
-    int     (*getattr)();
-    int     (*setattr)();
-    int     (*truncate)();
+    int     (*ilink)(struct dentry *oldname, inode_t *dir, struct dentry *newname);
+    int     (*ibind)(inode_t *dir, struct dentry *dentry, inode_t *ip);
+    int     (*irmdir)(inode_t *ip);
+    int     (*isync)(inode_t *ip);
+    int     (*iclose)(inode_t *ip);
+    int     (*iunlink)(inode_t *ip);
+    int     (*itruncate)(inode_t *ip);
+    ssize_t (*iread)(inode_t *ip, __off_t off, void *buf, size_t nb);
+    ssize_t (*iwrite)(inode_t *ip, __off_t off, void *buf, size_t nb);
+    int     (*imknod)(inode_t *dir, struct dentry *dentry, mode_t mode, int devid);
+    int     (*ifcntl)(inode_t *ip, int cmd, void *argp);
+    int     (*iioctl)(inode_t *ip, int req, void *argp);
+    int     (*imkdir)(inode_t *dir, struct dentry *dentry, mode_t mode);
+    int     (*ilookup)(inode_t *dir, struct dentry *dentry);
+    int     (*icreate)(inode_t *dir, struct dentry *dentry, mode_t mode);
+    int     (*irename)(inode_t *dir, struct dentry *old, inode_t *newdir, struct dentry *new);
+    ssize_t (*ireaddir)(inode_t *dir, off_t off, void *buf, size_t count);
+    int     (*isymlink)(inode_t *ip, inode_t *atdir, const char *symname);
+    int     (*igetattr)(inode_t *ip, void *attr);
+    int     (*isetattr)(inode_t *ip, void *attr);
 } iops_t;
 
 #define iassert(ip)         ({ assert((ip), "No inode"); })
@@ -53,24 +58,54 @@ typedef struct iops {
 #define ilocked(ip)         ({ iassert(ip); spin_islocked(&(ip)->i_lock); })
 #define iassert_locked(ip)  ({ iassert(ip); spin_assert_locked(&(ip)->i_lock); })
 
+#define icheck_op(ip, func) ({                      \
+    int err = 0;                              \
+    iassert_locked(ip);                       \
+    if ((ip) == NULL)                         \
+        err = -EINVAL;                        \
+    else if ((ip)->i_ops == NULL)             \
+        err = -EINVAL;                        \
+    else if ((ip)->i_ops->func == NULL) \
+        err = -ENOSYS;                        \
+    err;                                      \
+})
+
+#define IISTYPE(ip, type) ({\
+    iassert_locked(ip);\
+    (ip)->i_type == (type);\
+})
+
+#define IISINV(ip) ({ IISTYPE(ip, FS_INV); })
+#define IISREG(ip) ({ IISTYPE(ip, FS_REG); })
+#define IISDIR(ip) ({ IISTYPE(ip, FS_DIR); })
+#define IISCHR(ip) ({ IISTYPE(ip, FS_CHR); })
+#define IISSYM(ip) ({ IISTYPE(ip, FS_SYM); })
+#define IISBLK(ip) ({ IISTYPE(ip, FS_BLK); })
+#define IISFIFO(ip)({ IISTYPE(ip, FS_FIFO); })
+#define IISDEV(ip) ({ IISCHR(ip) || IISBLK(ip); })
+
 int ialloc(inode_t **pip);
+void iputcnt(inode_t *ip);
+void idupcnt(inode_t *ip);
+void iputlink(inode_t *ip);
+void iduplink(inode_t *ip);
 
-
-
-int     ilink(inode_t *ip);
+int     isync(inode_t *ip);
 int     iclose(inode_t *ip);
+int     iunlink(inode_t *ip);
+int     ibind(inode_t *dir, struct dentry *dentry, inode_t *ip);
+int     ilink(struct dentry *oldname, inode_t *dir, struct dentry *newname);
 ssize_t iread(inode_t *ip, __off_t off, void *buf, size_t nb);
 ssize_t iwrite(inode_t *ip, __off_t off, void *buf, size_t nb);
-int     imknod(inode_t *ip);
-int     ifnctl(inode_t *ip);
-int     iioctl(inode_t *ip);
-int     imkdir(inode_t *ip);
-int     iunlink(inode_t *ip);
-int     ilookup(inode_t *ip);
-int     icreate(inode_t *ip);
-int     irename(inode_t *ip);
-int     ireaddir(inode_t *ip);
-int     isymlink(inode_t *ip);
-int     igetattr(inode_t *ip);
-int     isetattr(inode_t *ip);
+int     imknod(inode_t *dir, struct dentry *dentry, mode_t mode, int devid);
+int     ifcntl(inode_t *ip, int cmd, void *argp);
+int     iioctl(inode_t *ip, int req, void *argp);
+int     imkdir(inode_t *dir, struct dentry *dentry, mode_t mode);
+int     ilookup(inode_t *dir, struct dentry *dentry);
+int     icreate(inode_t *dir, struct dentry *dentry, mode_t mode);
+int     irename(inode_t *dir, struct dentry *old, inode_t *newdir, struct dentry *new);
+ssize_t ireaddir(inode_t *dir, off_t off, void *buf, size_t count);
+int     isymlink(inode_t *ip, inode_t *atdir, const char *symname);
+int     igetattr(inode_t *ip, void *attr);
+int     isetattr(inode_t *ip, void *attr);
 int     itruncate(inode_t *ip);
---
+++ inode.c
@@ -0,0 +1,280 @@
+#include <inode.h>
+#include <fs.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <dentry.h>
+
+int ialloc(inode_t **pip) {
+    int err = -ENOMEM;
+    inode_t *ip = NULL;
+
+    if (pip == NULL)
+        return -EINVAL;
+
+    if ((ip = malloc(sizeof *ip)) == NULL)
+        return err;
+
+    memset(ip, 0, sizeof *ip);
+
+    ip->i_lock = SPINLOCK_INIT();
+    ip->i_count = 1;
+    ilock(ip);
+    *pip = ip;
+
+    return 0;
+}
+
+void ifree(inode_t *ip) {
+    iassert_locked(ip);
+
+    if (ip->i_count <= 0) {
+        iunlink(ip);
+        free(ip);
+    }
+}
+
+void idupcnt(inode_t *ip) {
+    iassert_locked(ip);
+    ip->i_count++;
+}
+
+void iputcnt(inode_t *ip) {
+    iassert_locked(ip);
+    ip->i_count--;
+}
+
+void iputlink(inode_t *ip) {
+    iassert_locked(ip);
+    ip->i_links--;
+}
+
+void iduplink(inode_t *ip) {
+    iassert_locked(ip);
+    ip->i_links++;
+}
+
+int     ibind(inode_t *dir, struct dentry *dentry, inode_t *ip) {
+    int err = 0;
+    iassert_locked(dir);
+
+    if (IISDIR(dir) == 0)
+        return -ENOTDIR;
+    
+    if ((err = icheck_op(dir, ibind)))
+        return err;
+    
+    return dir->i_ops->ibind(dir, dentry, ip);
+}
+
+int     isync(inode_t *ip) {
+    int err = 0;
+    iassert_locked(ip);
+
+    if ((err = icheck_op(ip, isync)))
+        return err;
+    
+    return ip->i_ops->isync(ip);
+}
+
+int     ilink(struct dentry *oldname, inode_t *dir, struct dentry *newname) {
+    int err = 0;
+    iassert_locked(dir);
+
+    if (IISDIR(dir) == 0)
+        return -ENOTDIR;
+
+    if ((err = icheck_op(dir, ilink)))
+        return err;
+    
+    return dir->i_ops->ilink(oldname, dir, newname);
+}
+
+int     iclose(inode_t *ip) {
+    int err = 0;
+    iassert_locked(ip);
+
+    if ((err = icheck_op(ip, iclose)))
+        return err;
+    
+    return ip->i_ops->iclose(ip);
+}
+
+ssize_t iread(inode_t *ip, __off_t off, void *buf, size_t nb) {
+    ssize_t err = 0;
+    iassert_locked(ip);
+
+    if (IISDIR(ip))
+        return -EISDIR;
+
+    if ((err = icheck_op(ip, iread)))
+        return err;
+    
+    return ip->i_ops->iread(ip, off, buf, nb);
+}
+
+ssize_t iwrite(inode_t *ip, __off_t off, void *buf, size_t nb) {
+    ssize_t err = 0;
+    iassert_locked(ip);
+
+    if (IISDIR(ip))
+        return -EISDIR;
+
+    if ((err = icheck_op(ip, iwrite)))
+        return err;
+    
+    return ip->i_ops->iwrite(ip, off, buf, nb);
+}
+
+int     imknod(inode_t *dir, struct dentry *dentry, mode_t mode, int devid) {
+    int err = 0;
+    iassert_locked(dir);
+
+    if (IISDIR(dir) == 0)
+        return -ENOTDIR;
+
+    if ((err = icheck_op(dir, imknod)))
+        return err;
+    
+    return dir->i_ops->imknod(dir, dentry, mode, devid);
+}
+
+int     ifcntl(inode_t *ip, int cmd, void *argp) {
+    int err = 0;
+    iassert_locked(ip);
+    if ((err = icheck_op(ip, ifcntl)))
+        return err;
+    
+    return ip->i_ops->ifcntl(ip, cmd, argp);
+}
+
+int     iioctl(inode_t *ip, int req, void *argp) {
+    int err = 0;
+    iassert_locked(ip);
+
+    if (IISDIR(ip))
+        return -EISDIR;
+
+    if ((err = icheck_op(ip, iioctl)))
+        return err;
+    
+    return ip->i_ops->iioctl(ip, req, argp);
+}
+
+int     imkdir(inode_t *dir, struct dentry *dentry, mode_t mode) {
+    int err = 0;
+    iassert_locked(dir);
+    
+    if (IISDIR(dir) == 0)
+        return -ENOTDIR;
+
+    if ((err = icheck_op(dir, imkdir)))
+        return err;
+    
+    return dir->i_ops->imkdir(dir, dentry, mode);
+}
+
+int     iunlink(inode_t *ip) {
+    int err = 0;
+    iassert_locked(ip);
+
+    if ((err = icheck_op(ip, iunlink)))
+        return err;
+    
+    return ip->i_ops->iunlink(ip);
+}
+
+int     ilookup(inode_t *dir, struct dentry *dentry) {
+    int err = 0;
+    
+    iassert_locked(dir);
+
+    if (IISDIR(dir) == 0)
+        return -ENOTDIR;
+
+    if ((err = icheck_op(dir, ilookup)))
+        return err;
+    
+    return dir->i_ops->ilookup(dir, dentry);
+}
+
+int     icreate(inode_t *dir, struct dentry *dentry, mode_t mode) {
+    int err = 0;
+    iassert_locked(dir);
+
+    if (IISDIR(dir) == 0)
+        return -ENOTDIR;
+
+    if ((err = icheck_op(dir, icreate)))
+        return err;
+    
+    return dir->i_ops->icreate(dir, dentry, mode);
+}
+
+int     irename(inode_t *dir, struct dentry *old, inode_t *newdir, struct dentry *new) {
+    int err = 0;
+    iassert_locked(dir);
+    if ((err = icheck_op(dir, irename)))
+        return err;
+    
+    return dir->i_ops->irename(dir, old, newdir, new);
+}
+
+ssize_t     ireaddir(inode_t *dir, off_t off, void *buf, size_t count) {
+    ssize_t err = 0;
+    iassert_locked(dir);
+
+    if (IISDIR(dir) == 0)
+        return -ENOTDIR;
+
+    if (buf == NULL)
+        return -EINVAL;
+
+    if (off < 0)
+        return -ERANGE;
+
+    if ((err = icheck_op(dir, ireaddir)))
+        return err;
+    
+    return dir->i_ops->ireaddir(dir, off, buf, count);
+}
+
+int     isymlink(inode_t *ip, inode_t *atdir, const char *symname) {
+    int err = 0;
+    iassert_locked(ip);
+    if ((err = icheck_op(ip, isymlink)))
+        return err;
+    
+    return ip->i_ops->isymlink(ip, atdir, symname);
+}
+
+int     igetattr(inode_t *ip, void *attr) {
+    int err = 0;
+    iassert_locked(ip);
+    if ((err = icheck_op(ip, igetattr)))
+        return err;
+    
+    return ip->i_ops->igetattr(ip, attr);
+}
+
+int     isetattr(inode_t *ip, void *attr) {
+    int err = 0;
+    iassert_locked(ip);
+    if ((err = icheck_op(ip, isetattr)))
+        return err;
+    
+    return ip->i_ops->isetattr(ip, attr);
+}
+
+int     itruncate(inode_t *ip) {
+    int err = 0;
+    iassert_locked(ip);
+
+    if (IISDIR(ip))
+        return -EISDIR;
+
+    if ((err = icheck_op(ip, itruncate)))
+        return err;
+    
+    return ip->i_ops->itruncate(ip);
+}
